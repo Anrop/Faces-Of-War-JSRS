0b880e6192fc68f73e95e3deace3582f5936ac1e
Björn Dahlgren <bjorn@dahlgren.at>
2019-09-29 21:33:10 +0200

    PPK

---
+++ addons/weapons/ppk/config.cpp
@@ -0,0 +1,45 @@
+class CfgPatches {
+    class anrop_fow_jsrs_weapons_ppk {
+        name = "anrop_fow_jsrs_weapons_ppk";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {"fow_weapons_c", "jsrs_soundmod_complete_edition"};
+        addonRootClass = "fow_weapons_c";
+        author = "Anrop";
+        url = "https://www.anrop.se";
+    };
+};
+
+class Mode_SemiAuto;
+
+class CfgWeapons 
+{
+    class Pistol_Base_F;
+    
+    class fow_w_ppk : Pistol_Base_F 
+    {
+        bullet1[] = {"\jsrs_soundmod_complete\JSRS_Soundmod_Soundfiles\weapons\shells\small\metal_1.ogg", 2.0099, 1, 10};
+        bullet2[] = {"\jsrs_soundmod_complete\JSRS_Soundmod_Soundfiles\weapons\shells\small\metal_2.ogg", 2.0099, 1, 10};
+        bullet3[] = {"\jsrs_soundmod_complete\JSRS_Soundmod_Soundfiles\weapons\shells\small\metal_3.ogg", 2.0099, 1, 10};
+        bullet4[] = {"\jsrs_soundmod_complete\JSRS_Soundmod_Soundfiles\weapons\shells\small\metal_4.ogg", 2.0099, 1, 10};
+        bullet5[] = {"\jsrs_soundmod_complete\JSRS_Soundmod_Soundfiles\weapons\shells\small\dirt_1.ogg", 2.0099, 1, 10};
+        bullet6[] = {"\jsrs_soundmod_complete\JSRS_Soundmod_Soundfiles\weapons\shells\small\dirt_2.ogg", 2.0099, 1, 10};
+        bullet7[] = {"\jsrs_soundmod_complete\JSRS_Soundmod_Soundfiles\weapons\shells\small\dirt_3.ogg", 2.0099, 1, 10};
+        bullet8[] = {"\jsrs_soundmod_complete\JSRS_Soundmod_Soundfiles\weapons\shells\small\dirt_4.ogg", 2.0099, 1, 10};
+        bullet9[] = {"\jsrs_soundmod_complete\JSRS_Soundmod_Soundfiles\weapons\shells\small\grass_1.ogg", 2.0099, 1, 10};
+        bullet10[] = {"\jsrs_soundmod_complete\JSRS_Soundmod_Soundfiles\weapons\shells\small\grass_2.ogg", 2.0099, 1, 10};
+        bullet11[] = {"\jsrs_soundmod_complete\JSRS_Soundmod_Soundfiles\weapons\shells\small\grass_3.ogg", 2.0099, 1, 10};
+        bullet12[] = {"\jsrs_soundmod_complete\JSRS_Soundmod_Soundfiles\weapons\shells\small\grass_4.ogg", 2.0099, 1, 10};
+        soundbullet[] = {"bullet1", 0.08, "bullet2", 0.084, "bullet3", 0.084, "bullet4", 0.084, "bullet5", 0.093, "bullet6", 0.093, "bullet7", 0.074, "bullet8", 0.074, "bullet9", 0.084, "bullet10", 0.085, "bullet11", 0.083, "bullet12", 0.083};
+        
+        class Single: Mode_SemiAuto
+        {
+            class BaseSoundModeType;
+            class StandardSound: BaseSoundModeType
+            {
+                soundSetShot[] = {"jsrs_Luger_shot_soundset", "jsrs_ww2_pistol_reverb_soundset"};
+            };
+        };
+    };
+};
