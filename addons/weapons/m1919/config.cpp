bd9a9abc1c6fee578fb611166d564fcde69095cd
Björn Dahlgren <bjorn@dahlgren.at>
2019-09-29 21:25:18 +0200

    M1919

---
+++ addons/weapons/m1919/config.cpp
@@ -0,0 +1,46 @@
+class CfgPatches {
+    class anrop_fow_jsrs_weapons_m1919 {
+        name = "anrop_fow_jsrs_weapons_m1919";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {"fow_weapons_c", "jsrs_soundmod_complete_edition"};
+        addonRootClass = "fow_weapons_c";
+        author = "Anrop";
+        url = "https://www.anrop.se";
+    };
+};
+
+class Mode_FullAuto;
+
+class CfgWeapons 
+{
+    class fow_rifle_base;
+    
+    class fow_w_m1919: fow_rifle_base 
+    {
+        class FullAuto: Mode_FullAuto
+        {
+            class BaseSoundModeType;
+            class StandardSound: BaseSoundModeType
+            {
+                soundSetShot[] = {"jsrs_browning_shot_soundset", "jsrs_ww2_mg_reverb_soundset"};
+            };
+        };
+    };
+
+    class HMG_M2;
+    class MGun;
+
+    class fow_w_m1919a4_mounted: HMG_M2
+    {
+        class manual: MGun
+        {
+            class BaseSoundModeType;
+            class StandardSound: BaseSoundModeType
+            {
+                soundSetShot[] = {"jsrs_browning_shot_soundset", "jsrs_ww2_mg_reverb_soundset"};
+            };
+        };
+    };
+};
